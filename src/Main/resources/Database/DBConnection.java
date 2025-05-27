fc57e3362f41199089da20a1de0d7ed14f992009
JoaoTeixeira <1241932@isep.ipp.pt>
2025-05-27 21:54:31 +0100

    Update DB server

--- src/Main/resources/Database/DBConnection.java
+++ src/Main/resources/Database/DBConnection.java
@@ -10,11 +10,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7780078";
-    private static final String USER = "sql7780078";
-    private static final String PASSWORD = "bz1R5hCj1N";
+    private static final String URL = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7781596";
+    private static final String USER = "sql7781596";
+    private static final String PASSWORD = "D8GRzJkduq";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com:3306/sql7780078", "sql7780078", "bz1R5hCj1N");
+        return DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com:3306/sql7781596", "sql7781596", "D8GRzJkduq");
     }
 }
+
+
